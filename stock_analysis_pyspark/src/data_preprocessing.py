9cda829baf9d0f06e47ad1c018403ad47ac6ca7e
KubiakJakub01 <kubiak.jakub01@gmail.com>
2023-11-25 13:06:08 +0100

    Save data to cassandra db

--- stock_analysis_pyspark/src/data_preprocessing.py
+++ stock_analysis_pyspark/src/data_preprocessing.py
@@ -1,16 +1,22 @@
+import logging
 import os
 from typing import Literal
 
+from cassandra.cluster import Cluster
 from pyspark.sql.functions import date_format
 from pyspark.sql.types import (
+    BooleanType,
+    DataType,
+    DoubleType,
     FloatType,
     IntegerType,
     StringType,
     StructField,
     StructType,
     TimestampType,
-    DataType
 )
+
+logger = logging.getLogger(__name__)
 
 
 def load_data(spark_session, data_path):
@@ -30,14 +36,65 @@ def load_data(spark_session, data_path):
 
 def write_data(df, path, target=Literal["csv", "cassandra"]):
     """Write data to csv file"""
+    df = df.toDF(*[c.lower() for c in df.columns])
     if target == "csv":
+        logger.info(f"Saving data to {path}")
         os.makedirs(os.path.dirname(path), exist_ok=True)
         df = df.withColumn("timestamp", date_format("timestamp", "yyyy/MM/dd"))
         df.write.csv(path=path, mode="overwrite", header=True)
     elif target == "cassandra":
+        logger.info(f"Saving data to Cassandra")
+        create_cassandra_table_from_df(
+            df, "localhost", "stock_analysis", "stock_prices"
+        )
         df.write.format("org.apache.spark.sql.cassandra").options(
             table="stock_prices", keyspace="stock_analysis"
-        ).mode("overwrite").save()
+        ).mode("append").save()
+
+
+def pyspark_to_cassandra_type(pyspark_type):
+    """
+    Maps PySpark data types to Cassandra data types.
+    """
+    type_mapping = {
+        StringType: "text",
+        IntegerType: "int",
+        FloatType: "float",
+        TimestampType: "timestamp",
+        BooleanType: "boolean",
+        DoubleType: "double",
+    }
+    return type_mapping.get(type(pyspark_type), "text")
+
+
+def create_cassandra_table_from_df(df, host, keyspace_name, table_name):
+    """Create a Cassandra table based on the DataFrame schema."""
+    schema = df.schema
+    columns = [
+        f"{field.name.lower()} {pyspark_to_cassandra_type(field.dataType)}"
+        for field in schema.fields
+    ]
+    logger.info(f"Creating Cassandra table {table_name} with schema: {columns}")
+
+    # Assuming the first column is the primary key for simplicity
+    primary_key = schema.fields[0].name if schema.fields else ""
+
+    # Construct the CREATE TABLE statement
+    create_table_statement = f"CREATE TABLE IF NOT EXISTS {table_name} ({', '.join(columns)}, PRIMARY KEY ({primary_key}))"
+
+    # Create keyspace and table
+    logger.info(f"Creating Cassandra keyspace {keyspace_name} and table {table_name}")
+    cluster = Cluster([host], port=9042)
+    session = cluster.connect()
+
+    session.execute(
+        f"CREATE KEYSPACE IF NOT EXISTS {keyspace_name} WITH replication = {{'class': 'SimpleStrategy', 'replication_factor': 1}}"
+    )
+    session.set_keyspace(keyspace_name)
+    session.execute(create_table_statement)
+
+    session.shutdown()
+    cluster.shutdown()
 
 
 def clean_data(df, fill_strategy=Literal["drop", "zeroes", "mean"]):
